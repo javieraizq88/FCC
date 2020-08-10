2ad735706603563bb17065a7c88f7aa578577286
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-08-10 17:10:55 -0400

    js

---
+++ JS/Basic-JavaScript/04-Storing Values with the Assignment Operator.js
@@ -0,0 +1,33 @@
+
+    // <h1>Storing Values with the Assignment Operator</h1>
+
+    // <h2>DESCRIPTION</h2>
+    // <p>In JavaScript, you can store a value in a variable with the assignment operator.</p>
+
+    // <p> myVariable = 5;</p>
+        
+    // <p> This assigns the Number value 5 to myVariable.</p>
+        
+    // <p> Assignment always goes from right to left. Everything to the right of the = operator is resolved before the value is assigned to the variable to the left of the operator.</p>
+        
+    // <p> myVar = 5;</p>
+    // <p> myNum = myVar;</p>
+    // <p> This assigns 5 to myVar and then resolves myVar to 5 again and assigns it to myNum.</p>
+
+
+    // <h2>INSTRUCTIONS</h2>
+    // <p>Assign the value 7 to variable a.</p>
+
+    // <p> Assign the contents of a to variable b.</p>
+
+
+    <h2>SOLUTION</h2>
+    // Setup
+    var a;
+    var b = 2;
+    
+    // Only change code below this line
+    var a = 7;
+    var b = a;
+
+
