c2e8e57193b1a2cbb6dd372f628d6ff7ebe3f045
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-08-11 20:19:45 -0400

    js

---
+++ JS/Basic-JavaScript/08-Add Two Numbers with JavaScript.js
@@ -0,0 +1,21 @@
+
+    // <h1>Add Two Numbers with JavaScript</h1>
+
+    // <h2>DESCRIPTION</h2>
+    // Number is a data type in JavaScript which represents numeric data.
+
+    // Now let's try to add two numbers using JavaScript.
+    
+    // JavaScript uses the + symbol as an addition operator when placed between two numbers.
+    
+    // Example:
+    
+    // myVar = 5 + 10; // assigned 15
+    
+    
+    // <h2>INSTRUCTIONS</h2>
+    // Change the 0 so that sum will equal 20.
+
+
+    <h2>SOLUTION</h2>
+    var sum = 10 + 10;
