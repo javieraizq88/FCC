c3dcda25c7dbdbbb9070f5103d44ff392c4df964
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-08-19 12:41:30 -0400

    js

---
+++ JS/Basic-JavaScript/11-Divide One Number by Another with JavaScript.js
@@ -0,0 +1,21 @@
+/*
+Divide One Number by Another with JavaScript
+
+    DESCRIPTION
+We can also divide one number by another.
+
+JavaScript uses the / symbol for division.
+
+Example
+
+myVar = 16 / 2; // assigned 8
+
+
+    INSTRUCTIONS
+Change the 0 so that the quotient is equal to 2.
+
+
+
+    SOLUTION */
+    var quotient = 66 / 33;
+
